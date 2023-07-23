a785d6c8395499cf0c72890cc76b336512e8cc11
sounan168 <csoonan4@gmail.com>
2023-07-23 16:57:26 +0700

    new scan

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Scan from './Scan';
 function App() {
   const [result, setresult] = useState('');
   function onNewScanResult (rs){
-    setresult(rs)
+    alert(rs)
   }
   return (
     <div className="App">
-      {result}
-      <Scan      fps={10}
+     
+      {/* <Scan      fps={10}
                 qrbox={250}
                 disableFlip={false}
                 qrCodeSuccessCallback={onNewScanResult}
-                aspectRatio={1}/>
+                aspectRatio={1}
+                /> */}
+      <Scan/>
+                
     
     </div>
   );
--- src/Scan.js
+++ src/Scan.js
@@ -1,60 +1,109 @@
-import { Html5QrcodeScanner } from "html5-qrcode";
-import React from 'react';
-const qrcodeRegionId = "html5qr-code-full-region";
+// import { Html5QrcodeScanner } from "html5-qrcode";
+// import React from 'react';
+// const qrcodeRegionId = "html5qr-code-full-region";
 
-class Scan extends React.Component {
+// class Scan extends React.Component {
   
-    render() {
-        // let rs = this.context
-        return (
-          <>
-          {/* {rs.result.UserID} */}
-            <div id={qrcodeRegionId} />
-          </>
-        );
-    }
+//     render() {
+//         // let rs = this.context
+//         return (
+//           <>
+//           {/* {rs.result.UserID} */}
+//             <div id={qrcodeRegionId} />
+//           </>
+//         );
+//     }
     
-    componentWillUnmount() {
-        // TODO(mebjas): See if there is a better way to handle
-        //  promise in `componentWillUnmount`.
-        this.html5QrcodeScanner.clear().catch(error => {
-            console.error("Failed to clear html5QrcodeScanner. ", error);
-        });
-    }
+//     componentWillUnmount() {
+//         // TODO(mebjas): See if there is a better way to handle
+//         //  promise in `componentWillUnmount`.
+//         this.html5QrcodeScanner.clear().catch(error => {
+//             console.error("Failed to clear html5QrcodeScanner. ", error);
+//         });
+//     }
 
-    componentDidMount() {
-        // Creates the configuration object for Html5QrcodeScanner.
-        function createConfig(props) {
-            var config = {focusMode:'continous',advanced:[{zoom: 1.5}],facingMode: "1" };
-            if (props.fps) {
-            config.fps = props.fps;
-            }
-            if (props.qrbox) {
-            config.qrbox = props.qrbox;
-            }
-            if (props.aspectRatio) {
-            config.aspectRatio = props.aspectRatio;
-            }
-            if (props.disableFlip !== undefined) {
-            config.disableFlip = props.disableFlip;
-            }
+//     componentDidMount() {
+//         // Creates the configuration object for Html5QrcodeScanner.
+//         function createConfig(props) {
+//             var config = {focusMode:'continous',advanced:[{zoom: 1.5}],facingMode: "1" };
+//             if (props.fps) {
+//             config.fps = props.fps;
+//             }
+//             if (props.qrbox) {
+//             config.qrbox = props.qrbox;
+//             }
+//             if (props.aspectRatio) {
+//             config.aspectRatio = props.aspectRatio;
+//             }
+//             if (props.disableFlip !== undefined) {
+//             config.disableFlip = props.disableFlip;
+//             }
           
-            return config;
-        }
+//             return config;
+//         }
       
-        var config = createConfig(this.props);
-        var verbose = this.props.verbose === true;
+//         var config = createConfig(this.props);
+//         var verbose = this.props.verbose === true;
 
-        // Suceess callback is required.
-        if (!(this.props.qrCodeSuccessCallback )) {
-            // throw "qrCodeSuccessCallback is required callback.";
+//         // Suceess callback is required.
+//         if (!(this.props.qrCodeSuccessCallback )) {
+//             // throw "qrCodeSuccessCallback is required callback.";
+//         }
+        
+
+//         this.html5QrcodeScanner = new Html5QrcodeScanner(
+//             qrcodeRegionId, config, verbose);
+//         this.html5QrcodeScanner.render(
+//             this.props.qrCodeSuccessCallback,
+//             this.props.qrCodeErrorCallback);
+           
+//     }
+// };
+// export default Scan;
+
+import React, {useState} from 'react'
+// import { Link } from "react-router-dom";
+import {QrReader} from 'react-qr-reader'
+
+function QRscanner() {
+
+    const [qrscan, setQrscan] = useState(null);
+    const handleScan = data => {
+        if (data) {
+            setQrscan(data)
         }
-
-        this.html5QrcodeScanner = new Html5QrcodeScanner(
-            qrcodeRegionId, config, verbose);
-        this.html5QrcodeScanner.render(
-            this.props.qrCodeSuccessCallback,
-            this.props.qrCodeErrorCallback);
     }
-};
-export default Scan;
+    const handleError = err => {
+    console.error(err)
+    }
+var id =10
+    return (
+      <div>
+            {qrscan!=null?qrscan.user=id:'no'}
+            <span>QR Scanner</span>
+            
+            <center>
+                <div style={{width:'100%'}}>
+
+                <QrReader
+                    
+                    delay={300}
+                    constraints={{
+                        facingMode: 'environment'
+                    }}
+                    onError={handleError}
+                    onResult={handleScan}
+                    // onScan={handleScan}
+                    
+                />
+                </div>
+         
+            </center>
+
+       
+      </div>
+    );
+  }
+  
+  export default QRscanner;
+  
